7bc7cb0cc324ec1e598694fcad2dc12a9e0b91f9
albin derby <derbyalbin@gmail.com>
2025-08-14 20:44:13 +0530

    show todolis completed

--- display.js
+++ display.js
@@ -1,6 +1,6 @@
 // databaseilnn data retrive cheyannam ennit athh button ayitt left project baril show cheyannam
-import { fetchTodoFromProject, retrieveAll, STORE_NAMES } from "./indexedDB.js";
-import { createProjectButton,appendNewProjectOnLefSideBar, createDiv } from "./dom.js";
+import { fetchTodoFromProject, retrieveAll, retriveSpecificTodo, STORE_NAMES } from "./indexedDB.js";
+import { createProjectButton,appendNewProjectOnLefSideBar, createDiv, createTodo } from "./dom.js";
 import { currentProject } from "./main.js";
 export async function showProjectButton(){
     const allProjcetNames=await retrieveAll(STORE_NAMES.PROJECT);
@@ -22,8 +22,23 @@ export async function showTodoList(currentProject) {
        const toDoList=await fetchTodoFromProject(currentProject);
         for(let i=0;i<toDoList.length;i++){
             const div=createDiv(toDoList[i].title);
+             div.addEventListener("click",async (event)=>{
+                const toDo=event.target;
+                const listNo=toDo.dataset.listNo;
+                const specificTodo=await retriveSpecificTodo(Number(listNo));
+                console.log(specificTodo);
+                displayTodo(specificTodo);
+            })
             div.classList.add("list");
+            div.dataset.listNo=toDoList[i].list_no;
             rightSide.appendChild(div);       
         }
 
+}
+
+
+
+function displayTodo(todo){
+    const rightSide=document.getElementById("right-side");
+    rightSide.append(createTodo(todo));
 }
